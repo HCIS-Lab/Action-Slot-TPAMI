ed16e552fa6c15abf14c4455ef46bfb2c057b51b
Reezzz702 <changhowad@gmail.com>
2026-06-16 21:51:24 +0800

    init commit

--- scripts/train_loc.sh
+++ scripts/train_loc.sh
@@ -1,5 +1,4 @@
-export TORCH_DISTRIBUTED_DEBUG=INFO
-python train_localization.py --attn_cp ../weights/taco_action_slot_best_model.pth --cp ../weights/taco_action_slot_best_model.pth \
-        --root ../../data_collection/ --dataset taco --model_name action_slot --num_slots 64 --attn_model_name action_slot \
+python train_localization.py --attn_cp [/path/to/attn_source.pth] --cp [/path/to/recog_model.pth] \
+        --root [/path/to/taco_dataset] --dataset taco --model_name [recog model name] --num_slots 64 --attn_model_name [attn source model name] \
         --bg_slot --bg_mask --allocated_slot --seg_only --batch_size 4 --gpus 0 1 2 3 --val_every 2 --obj_mask \
-        --ref --pseudo_mask --refine --attn_backbone x3d --backbone x3d --decoder cross_attn
+        --ref --pseudo_mask --refine --attn_backbone [attn source model backbone] --backbone [recog model backbone] --decoder [localization decoder]
